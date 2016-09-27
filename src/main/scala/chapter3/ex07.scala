8dc3c48c5ba6e20f979d79058020a465c80707f2
kirill <kirill@eldis.ru>
2016-09-27 16:30:13 +0700

    глава 3 задача 07

--- src/main/scala/chapter3/ex07.scala
+++ src/main/scala/chapter3/ex07.scala
@@ -4,8 +4,6 @@
 
 package chapter3 {
 
-  import scala.collection.mutable.ArrayBuffer
-
   object ex07 extends App {
     assert(Array(1, 2, 3, 4, 2, 1, 3, 4, 1, 2, 3, 4, 1, 1,1 ,2).distinct.deep   == Array(1, 2, 3, 4).deep)
     assert(Array(3, 1, 3, 4, 1, 2, -1, 0, 3, -2, -1).distinct.deep == Array(3, 1, 4, 2, -1, 0, -2).deep)
