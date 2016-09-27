3031a77dba89917a8587716be3fcac4c36cf1044
kirill <kirill@eldis.ru>
2016-09-27 09:54:50 +0700

    глава 2 задача 7

--- src/main/scala/chapter2/ex7.scala
+++ src/main/scala/chapter2/ex7.scala
@@ -3,7 +3,10 @@
   */
 package  chapter2 {
   object ex7 extends App {
-    for (i <- 0 to 10) println(10 - i)
+  def prod(desc:String) :BigInt ={
+    desc.map(BigInt(_)).product
   }
+
+  assert(prod("Hello") == 9415087488L)  }
 }
 
