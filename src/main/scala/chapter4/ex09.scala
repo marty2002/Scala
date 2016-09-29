52c0b732a4427bfa021c7171c5a7b54fa2e75cc1
kirill <kirill@eldis.ru>
2016-09-29 08:53:40 +0700

    глава 4 задача 09

--- src/main/scala/chapter4/ex09.scala
+++ src/main/scala/chapter4/ex09.scala
@@ -2,3 +2,12 @@
   * Created by kirill on 29.09.2016.
   */
 
+object ex09 extends App {
+  def lteqgt(values: Array[Int], v: Int): (Int, Int, Int) = {
+    (values.count(_<v), values.count(_==v), values.count(_>v))
+  }
+
+  assert(lteqgt(Array( 1,  2,  3,  4,  5,  6),  3) == (2, 1, 3))
+  assert(lteqgt(Array(-1, -2, -3, -4, -5, -6), -3) == (3, 1, 2))
+}
+
