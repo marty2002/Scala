4e3c4b0e9ac6624999243b339be421a5c1062b8a
kirill <kirill@eldis.ru>
2016-09-29 08:45:14 +0700

    глава 4 задача 08

--- src/main/scala/chapter4/ex08.scala
+++ src/main/scala/chapter4/ex08.scala
@@ -2,8 +2,6 @@
   * Created by kirill on 29.09.2016.
   */
 
-import scala.collection.JavaConversions.propertiesAsScalaMap
-
 object ex08 extends App {
   def minmax(values: Array[Int]): (Int, Int) = {
     (values.min, values.max)
