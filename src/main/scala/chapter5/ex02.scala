f2e8d393ea7e31627e1fd524a71c53085b090bd3
kirill <kirill@eldis.ru>
2016-09-29 12:05:43 +0700

    глава 5 задача 02

---
+++ src/main/scala/chapter5/ex02.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by kirill on 29.09.2016.
+  */
+
+package chapter5 {
+  object ex02 extends App {
+    class BankAccount (private val current: Int = 0) {
+      def deposit(v: Int) = new BankAccount(current+v)
+      def withdraw(v: Int)= new BankAccount(current-v)
+      def balance = current
+    }
+
+    assert(new BankAccount(100).deposit(30).withdraw(15).balance == 115)
+    assert(new BankAccount(0).deposit(30).withdraw(45).balance == -15)
+  }
+}
