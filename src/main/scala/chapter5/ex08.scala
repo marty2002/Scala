2f9397fc8a541510213f731b1b83cc64db021e0a
kirill <kirill@eldis.ru>
2016-09-30 09:20:34 +0700

    глава 5 задача 08

---
+++ src/main/scala/chapter5/ex08.scala
@@ -0,0 +1,37 @@
+/**
+  * Created by kirill on 29.09.2016.
+  */
+package chapter5 {
+
+  import com.sun.javafx.sg.PGShape.Mode
+
+  object ex08 extends App {
+    class Car (
+      private val m: String,
+      private val c: String,
+      private val y: Int = -1,
+      private val r: String = "") {
+
+      def this(model: String, corp:String) {
+        this(m=model, c=corp)
+      }
+      def this(model: String, corp: String, year: Int) {
+        this(m=model, c=corp, y=year)
+      }
+      def this(model: String, corp:String, regNum: String) {
+        this(m=model, c=corp, r=regNum)
+      }
+
+      def model  = m
+      def corp   = c
+      def year   = y
+      def regNum = r
+      def regNum(regNum: String): Car = new Car(model, corp, year, regNum)
+    }
+
+    assert(new Car("model", "corp", 1, "oldRegNum").regNum("newRegNum").regNum == "newRegNum")
+    assert(new Car("model", "corp", 1).regNum("newRegNum").regNum == "newRegNum")
+    assert(new Car("model", "corp", "oldRegNum").regNum("newRegNum").regNum == "newRegNum")
+    assert(new Car("model", "corp").regNum("newRegNum").regNum == "newRegNum")
+  }
+}
