65625fba0ea5f1f1039b88fd7328b00082aaaf38
kirill <kirill@eldis.ru>
2016-10-05 12:57:38 +0700

    глава 9 задача 05

--- src/main/scala/chapter9/ex05.scala
+++ src/main/scala/chapter9/ex05.scala
@@ -3,10 +3,13 @@
   */
 
 package chapter9 {
+  import java.io.PrintWriter
   object ex05 extends App {
+    val out = new PrintWriter("txt\\chapter9Ex05.txt", "UTF-8")
     (0 to 20).map(x=>{
       val l = math.pow(2, x).toInt
-      println(l.toString  + " " * (8 - l.toString.length) + math.pow(2, -x).toString)
+      out.println(l.toString  + " " * (8 - l.toString.length) + math.pow(2, -x).toString)
     })
+    out.close
   }
 }
